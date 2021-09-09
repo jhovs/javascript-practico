ee7b0eb437be7f50165d12584bbb25d61a01047e
jhovan <jhovan.solano@gmail.com>
2021-09-08 20:06:25 -0500

    Taller de figuras geometricas día 2

--- figuras.js
+++ figuras.js
@@ -1,13 +1,19 @@
 // Codigo del cuadrado 
 console.group("Cuadrado");
 
-const ladoCuadrado = 5;
-const perimetroCuadrado = ladoCuadrado * 4;
-const areaCuadrado = ladoCuadrado + ladoCuadrado;
+//const ladoCuadrado = 5;
 
-console.log(`Los lados del cuadrago miden: ${ladoCuadrado} cm`);
-console.log(`El perimetro del cuadrago es: ${perimetroCuadrado} cm`);
-console.log(`El área del cuadrago es: ${areaCuadrado} cm²`);
+function perimetroCuadrado(lado) {
+    return lado * 4;
+} 
+
+function areaCuadrado (lado){
+    return lado * lado;
+}
+
+//console.log(`Los lados del cuadrago miden: ${ladoCuadrado} cm`);
+//console.log(`El perimetro del cuadrago es: ${perimetroCuadrado} cm`);
+//console.log(`El área del cuadrago es: ${areaCuadrado} cm²`);
 
 
 console.groupEnd();
@@ -15,17 +21,12 @@ console.groupEnd();
 // Codigo del triangulo
 console.group("Triangulos");
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
+const perimetroTriangulo = (lado1,lado2,base) => lado1 + lado2 + base;
+const areaTriangulo = (base,altura) => (base * altura) / 2;
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-const areaTriangulo = baseTriangulo * alturaTriangulo / 2;
-
-console.log(`Los lados del triangulo ${ladoTriangulo1} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm`);
-console.log(`El perimetro del triangulo es: ${perimetroTriangulo} cm`);
-console.log(`El área del triangulo es: ${areaTriangulo} cm²`);
+//console.log(`Los lados del triangulo ${ladoTriangulo1} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm, altura ${alturaTriangulo} cm`);
+//console.log(`El perimetro del triangulo es: ${perimetroTriangulo} cm`);
+//console.log(`El área del triangulo es: ${areaTriangulo} cm²`);
 
 console.groupEnd();
 
@@ -33,16 +34,15 @@ console.groupEnd();
 // Codigo del circulo 
 console.group("Circulos");
 
-const diametroCirculo = 8;
-const radioCirculo = 4;
+//const radioCirculo = 4;
+//const diametroCirculo = radioCirculo * 2;
+//const pi = Math.PI;
 
-const perimetroCirculo = diametroCirculo * 3.1416;
-const areaCirculo = 3.1426 * (radioCirculo * radioCirculo);
+const perimetroCirculo = diametroCirculo * pi;
+const areaCirculo = (radioCirculo * radioCirculo) * pi;
 
-console.log(`El radio del circulo es ${radioCirculo} cm`);
-console.log(`El perimetro del circulo es: ${perimetroCirculo} cm`);
-console.log(`El área del circulo es: ${areaCirculo} cm²`);
-
-
+//console.log(`El radio del circulo es ${radioCirculo} cm, perimetro es ${diametroCirculo}`);
+//console.log(`El perimetro del circulo es: ${perimetroCirculo} cm`);
+//console.log(`El área del circulo es: ${areaCirculo} cm²`);
 
 console.groupEnd();
